a6030b62c9baecb77ba5cb7d787aa5ad55b001f2
Axel Brisse <axel.brisse@gmail.com>
2023-08-03 20:01:57 +0200

    rm unused import

--- frontend/src/AuthorizeUser.tsx
+++ frontend/src/AuthorizeUser.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
 
 const AuthorizeUser: React.FC = () => {
 	const navigate = useNavigate();
