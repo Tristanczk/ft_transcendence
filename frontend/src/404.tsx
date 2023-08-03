a0e9935dcc5e7a7db8154a8415112492adabadfd
Axel Brisse <axel.brisse@gmail.com>
2023-08-03 22:18:48 +0200

    404 dark by default

--- frontend/src/404.tsx
+++ frontend/src/404.tsx
@@ -2,21 +2,21 @@ import React from 'react';
 
 const NotFound: React.FC = () => {
     return (
-        <section className="bg-white dark:bg-gray-900">
+        <section className="bg-gray-900">
             <div className="container mx-auto min-h-screen px-6 py-12 lg:flex lg:items-center lg:gap-12 max-w-4xl">
                 <div className="w-full lg:w-1/2 text-center">
-                    <p className="text-sm font-medium text-blue-500 dark:text-blue-400">
+                    <p className="text-sm font-medium text-blue-400">
                         404 error
                     </p>
-                    <h1 className="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">
+                    <h1 className="mt-3 text-2xl font-semibold text-white md:text-3xl">
                         Page not found
                     </h1>
-                    <p className="mt-4 text-gray-500 dark:text-gray-400">
+                    <p className="mt-4 text-gray-400">
                         You seem to be lost. Here are some helpful links.
                     </p>
 
                     <div className="flex items-center mt-6 gap-x-3 justify-center">
-                        <button className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700">
+                        <button className="flex items-center justify-center w-1/2 px-5 py-2 text-sm transition-colors duration-200  border rounded-lg gap-x-2 sm:w-auto hover:bg-gray-800 bg-gray-900 text-gray-200 border-gray-700">
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 fill="none"
@@ -34,7 +34,7 @@ const NotFound: React.FC = () => {
                             <span>Go back</span>
                         </button>
 
-                        <button className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
+                        <button className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 rounded-lg shrink-0 sm:w-auto hover:bg-blue-500 bg-blue-600">
                             Take me home
                         </button>
                     </div>
