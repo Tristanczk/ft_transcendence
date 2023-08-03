6e30d915f85f852a8450604e6363b2733e0d187e
Axel Brisse <axel.brisse@gmail.com>
2023-08-03 22:27:29 +0200

    NavBar dark by default + DropLink

--- frontend/src/NavBar.tsx
+++ frontend/src/NavBar.tsx
@@ -13,12 +13,25 @@ const NavLink: React.FC<{ title: string; current: string; link: string }> = ({
                 href={link}
                 className={`block py-2 pl-3 pr-4 rounded ${
                     isActive
-                        ? 'text-white bg-blue-700 md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500'
-                        : 'text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
+                        ? 'text-white bg-blue-700 md:bg-transparent md:p-0 md:text-blue-500'
+                        : 'md:p-0 text-white md:hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700'
                 }`}
                 aria-current="page"
             >
                 {title}
+            </a>
+        </li>
+    );
+};
+
+const DropLink: React.FC<{ text: string }> = ({ text }) => {
+    return (
+        <li>
+            <a
+                href="/"
+                className="block px-4 py-2 text-sm hover:bg-gray-600 text-gray-200 hover:text-white"
+            >
+                {text}
             </a>
         </li>
     );
@@ -29,7 +42,7 @@ const NavBar: React.FC = () => {
     const location = useLocation();
 
     return (
-        <nav className="bg-white border-gray-200 dark:bg-gray-900">
+        <nav className="border-gray-200 bg-gray-900">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="/" className="flex items-center">
                     <img
@@ -37,14 +50,14 @@ const NavBar: React.FC = () => {
                         className="w-8 h-8 mr-3"
                         alt="lipong.org logo"
                     />
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
                         lipong.org
                     </span>
                 </a>
                 <div className="flex items-center md:order-2 relative">
                     <button
                         type="button"
-                        className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                        className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-600"
                         id="user-menu-button"
                         aria-expanded="false"
                         onClick={() => setDropdownOpen(!dropdownOpen)}
@@ -57,58 +70,30 @@ const NavBar: React.FC = () => {
                         />
                     </button>
                     <div
-                        className={`z-50 absolute top-full right-0 mt-2 w-48 py-2 bg-white rounded-md shadow-xl dark:bg-gray-800 ${
+                        className={`z-50 absolute top-full right-0 mt-2 w-48 py-2 rounded-md shadow-xl bg-gray-800 ${
                             dropdownOpen ? 'block' : 'hidden'
                         }`}
                         id="user-dropdown"
                     >
                         <div className="px-4 py-3">
-                            <span className="block text-sm text-gray-900 dark:text-white">
+                            <span className="block text-sm text-white">
                                 Bonnie Green
                             </span>
-                            <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">
+                            <span className="block text-sm truncate text-gray-400">
                                 name@flowbite.com
                             </span>
                         </div>
                         <ul className="py-2" aria-labelledby="user-menu-button">
-                            <li>
-                                <a
-                                    href="/"
-                                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                                >
-                                    Dashboard
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="/"
-                                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                                >
-                                    Settings
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="/"
-                                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                                >
-                                    Earnings
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="/"
-                                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                                >
-                                    Sign out
-                                </a>
-                            </li>
+                            <DropLink text="Dashboard" />
+                            <DropLink text="Settings" />
+                            <DropLink text="Earnings" />
+                            <DropLink text="Sign out" />
                         </ul>
                     </div>
                     <button
                         data-collapse-toggle="navbar-user"
                         type="button"
-                        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden focus:outline-none focus:ring-2 text-gray-400 hover:bg-gray-700 focus:ring-gray-600"
                         aria-controls="navbar-user"
                         aria-expanded="false"
                     >
@@ -134,7 +119,7 @@ const NavBar: React.FC = () => {
                     className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
                     id="navbar-user"
                 >
-                    <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-gray-800 md:bg-gray-900 border-gray-700">
                         <NavLink
                             current={location.pathname}
                             title="Home"
