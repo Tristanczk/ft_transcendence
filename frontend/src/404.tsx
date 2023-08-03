5ba172375c626e47b3c2612b1cea2d96b23b38ad
Axel Brisse <axel.brisse@gmail.com>
2023-08-03 22:11:05 +0200

    center the 404 page

--- frontend/src/404.tsx
+++ frontend/src/404.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 const NotFound: React.FC = () => {
     return (
         <section className="bg-white dark:bg-gray-900">
-            <div className="container min-h-screen px-6 py-12 mx-auto lg:flex lg:items-center lg:gap-12">
-                <div className="w-full lg:w-1/2">
+            <div className="container mx-auto min-h-screen px-6 py-12 lg:flex lg:items-center lg:gap-12 max-w-4xl">
+                <div className="w-full lg:w-1/2 text-center">
                     <p className="text-sm font-medium text-blue-500 dark:text-blue-400">
                         404 error
                     </p>
@@ -15,23 +15,22 @@ const NotFound: React.FC = () => {
                         You seem to be lost. Here are some helpful links.
                     </p>
 
-                    <div className="flex items-center mt-6 gap-x-3">
+                    <div className="flex items-center mt-6 gap-x-3 justify-center">
                         <button className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700">
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 fill="none"
                                 viewBox="0 0 24 24"
-                                stroke-width="1.5"
+                                strokeWidth="1.5"
                                 stroke="currentColor"
                                 className="w-5 h-5 rtl:rotate-180"
                             >
                                 <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
                                     d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
                                 />
                             </svg>
-
                             <span>Go back</span>
                         </button>
 
@@ -41,7 +40,7 @@ const NotFound: React.FC = () => {
                     </div>
                 </div>
 
-                <div className="relative w-full mt-12 lg:w-1/2 lg:mt-0">
+                <div className="relative w-full mt-12 lg:w-1/2 lg:mt-0 text-center flex justify-center items-center">
                     <img
                         className="w-full max-w-lg lg:mx-auto"
                         src="https://merakiui.com/images/components/illustration.svg"
