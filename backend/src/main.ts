d2cbd9f9222693cb8be8c745addc53dcdb746eaa
Axel Brisse <axel.brisse@gmail.com>
2023-08-03 20:27:32 +0200

    format all files

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,19 +4,21 @@ import { ValidationPipe } from '@nestjs/common';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-  }));
-  const corsOptions: CorsOptions = {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  };
+    const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+        }),
+    );
+    const corsOptions: CorsOptions = {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        credentials: true,
+    };
 
-  app.enableCors(corsOptions);
+    app.enableCors(corsOptions);
 
-  await app.listen(3333);
+    await app.listen(3333);
 }
 bootstrap();
