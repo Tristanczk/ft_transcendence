ccb90fa133a624dcd8eff92c95933388497aebc7
Axel Brisse <axel.brisse@gmail.com>
2023-08-03 21:04:13 +0200

    format main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,20 +5,15 @@ import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.int
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.useGlobalPipes(
-        new ValidationPipe({
-            whitelist: true,
-        }),
-    );
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
     const corsOptions: CorsOptions = {
         origin: 'http://localhost:3000',
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true,
     };
-
     app.enableCors(corsOptions);
-
     await app.listen(3333);
 }
+
 bootstrap();
