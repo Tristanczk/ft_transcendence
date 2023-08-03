7f3e5f9cb0530ff9a9d53b964bca2fba805acde1
tczarnia <tristanczk@yahoo.fr>
2023-08-03 15:00:50 +0200

    Navlink

--- frontend/src/NavBar.tsx
+++ frontend/src/NavBar.tsx
@@ -1,4 +1,25 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+
+const NavLink: React.FC<{ title: string; isActive: boolean }> = ({
+    title,
+    isActive,
+}) => {
+    return (
+        <li>
+            <a
+                href="#"
+                className={`block py-2 pl-3 pr-4 rounded ${
+                    isActive
+                        ? 'text-white bg-blue-700 md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500'
+                        : 'text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
+                }`}
+                aria-current="page"
+            >
+                {title}
+            </a>
+        </li>
+    );
+};
 
 const NavBar: React.FC = () => {
     return (
@@ -6,12 +27,12 @@ const NavBar: React.FC = () => {
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="https://flowbite.com/" className="flex items-center">
                     <img
-                        src="https://flowbite.com/docs/images/logo.svg"
+                        src="https://notion-emojis.s3-us-west-2.amazonaws.com/prod/svg-twitter/1f3d3.svg"
                         className="h-8 mr-3"
                         alt="Flowbite Logo"
                     />
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                        Flowbite
+                        lipong.org
                     </span>
                 </a>
                 <div className="flex items-center md:order-2">
@@ -107,47 +128,11 @@ const NavBar: React.FC = () => {
                     id="navbar-user"
                 >
                     <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                        <li>
-                            <a
-                                href="#"
-                                className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-                                aria-current="page"
-                            >
-                                Home
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href="#"
-                                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                            >
-                                About
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href="#"
-                                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                            >
-                                Services
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href="#"
-                                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                            >
-                                Pricing
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href="#"
-                                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                            >
-                                Contact
-                            </a>
-                        </li>
+                        <NavLink title="Home" isActive={true} />
+                        <NavLink title="About" isActive={false} />
+                        <NavLink title="Services" isActive={false} />
+                        <NavLink title="Pricing" isActive={false} />
+                        <NavLink title="Contact" isActive={false} />
                     </ul>
                 </div>
             </div>
