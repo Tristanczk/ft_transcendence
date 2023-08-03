eb620f1e43ab304dc02217a0ac29aca6c48c5aea
Axel Brisse <axel.brisse@gmail.com>
2023-08-04 00:05:39 +0200

    404.tsx to pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import RootPage from './pages/RootPage';
-import NotFound from './404';
+import NotFound from './pages/404';
 import Dashboard from './pages/DashboardPage';
 import SignIn from './pages/SignInPage';
 import Chat from './pages/ChatPage';
 import NavBar from './NavBar';
 import Settings from './pages/SettingsPage';
 
-const App: React.FC = () => {
-    return (
-        <BrowserRouter>
-            <NavBar />
-            <Routes>
-                <Route path="/" Component={RootPage} />
-                <Route path="/dashboard" Component={Dashboard} />
-                <Route path="/chat" Component={Chat} />
-                <Route path="/signin" Component={SignIn} />
-                <Route path="/settings" Component={Settings} />
-                <Route path="*" Component={NotFound} />
-            </Routes>
-        </BrowserRouter>
-    );
-};
+const App: React.FC = () => (
+    <BrowserRouter>
+        <NavBar />
+        <Routes>
+            <Route path="/" Component={RootPage} />
+            <Route path="/dashboard" Component={Dashboard} />
+            <Route path="/chat" Component={Chat} />
+            <Route path="/signin" Component={SignIn} />
+            <Route path="/settings" Component={Settings} />
+            <Route path="*" Component={NotFound} />
+        </Routes>
+    </BrowserRouter>
+);
 
 export default App;
