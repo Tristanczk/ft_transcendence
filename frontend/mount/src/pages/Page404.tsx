35929c779e332d48d83c4d36937e6214c32642d1
Axel Brisse <axel.brisse@gmail.com>
2023-08-04 20:44:11 +0200

    Center 404 on large screens

--- frontend/mount/src/pages/Page404.tsx
+++ frontend/mount/src/pages/Page404.tsx
@@ -14,8 +14,8 @@ const Page404: React.FC = () => {
     const navigate = useNavigate();
     return (
         <section className="bg-gray-900">
-            <div className="container mx-auto min-h-screen px-6 py-12 lg:flex lg:items-center lg:gap-12 max-w-4xl">
-                <div className="w-full lg:w-1/2 text-center">
+            <div className="container mx-auto min-h-screen px-6 py-12 max-w-4xl">
+                <div className="w-full text-center">
                     <p className="text-sm font-medium text-blue-400">
                         404 error
                     </p>
@@ -36,7 +36,7 @@ const Page404: React.FC = () => {
                         />
                     </div>
                 </div>
-                <div className="relative w-full mt-12 lg:w-1/2 lg:mt-0 text-center flex justify-center items-center">
+                <div className="relative w-full mt-12 text-center flex justify-center items-center">
                     <img
                         className="w-full max-w-lg lg:mx-auto"
                         src="/404.svg"
