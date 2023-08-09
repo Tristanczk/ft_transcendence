8e8fa2b4078fca77d896642dda0607f81914c2fc
Tristan Czarniak <tristanczk@yahoo.fr>
2023-08-09 15:50:14 +0200

    corrected schema prisma

--- backend/backend.sh
+++ backend/backend.sh
@@ -10,7 +10,7 @@ echo "-----Migrating database-----"
 while true; do
 	echo "-----Migrating database try-----"
 	# uncomment to reset database if error when using migrate dev (especially if columns have been deleted with values still in the db)
-	npx prisma migrate reset --force 
+	# npx prisma migrate reset --force 
 	npx prisma migrate dev --name init
 	EXIT_CODE=$?
 	echo "PRISMA EXIT CODE: $EXIT_CODE"
