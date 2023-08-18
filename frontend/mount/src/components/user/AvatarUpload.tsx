b40e37d335b85ea969acc2b3409a53f579dfecb1
Nicolas LESAGE <nlesage@paul-f5Br6s4.clusters.42paris.fr>
2023-08-18 16:24:50 +0200

    before backend for avatar images

--- frontend/mount/src/components/user/AvatarUpload.tsx
+++ frontend/mount/src/components/user/AvatarUpload.tsx
@@ -6,9 +6,9 @@ function AvatarUploader() {
   const [imagePreview, setImagePreview] = useState<string | null>(null);
 
   const handleImageChange = async (e: ChangeEvent<HTMLInputElement>) => {
-	console.log(e)
+	// console.log(e)
     const selectedImage = e.target.files?.[0];
-	console.log(selectedImage)
+	// console.log(selectedImage)
     if (selectedImage) {
       const reader = new FileReader();
       reader.onload = () => {
@@ -16,25 +16,25 @@ function AvatarUploader() {
       };
       reader.readAsDataURL(selectedImage);
 	
-	  console.log(imagePreview)
-	  console.log(reader)
+	//   console.log(imagePreview)
+	//   console.log(reader)
     }
 
-	// if (selectedImage) {
-	// 	let formData = new FormData();
-	// 	console.log('try send photo')
-	// 	formData.append("image", selectedImage);
-	// 	try {
-	// 		const response = await axios.post(
-	// 			`http://localhost:3333/users/avatar`,
-	// 			formData,
-	// 			{ withCredentials: true },
-	// 		);
-	// 		console.log(response)
-	// 	} catch (error) {
-	// 		console.error(error);
-	// 	}
-	// }
+	if (selectedImage) {
+		let formData = new FormData();
+		console.log('try send photo')
+		formData.append("image", selectedImage);
+		try {
+			const response = await axios.post(
+				`http://localhost:3333/users/avatar`,
+				formData,
+				{ withCredentials: true },
+			);
+			console.log(response)
+		} catch (error) {
+			console.error(error);
+		}
+	}
 	
   };
 
