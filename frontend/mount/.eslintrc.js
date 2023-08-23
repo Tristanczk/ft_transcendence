aabad3996925a21faabcf6e10e24368624efb0a4
Nicolas <nicolas.lesage@hotmail.fr>
2023-08-23 09:45:35 +0200

    adding prettier for front

--- frontend/mount/.eslintrc.js
+++ frontend/mount/.eslintrc.js
@@ -1,13 +1,14 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  // parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  // plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['prettier'],
   extends: [
-    'plugin:@typescript-eslint/recommended',
+    // 'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
   root: true,
@@ -17,9 +18,10 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    // '@typescript-eslint/interface-name-prefix': 'off',
+    // '@typescript-eslint/explicit-function-return-type': 'off',
+    // '@typescript-eslint/explicit-module-boundary-types': 'off',
+    // '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': 'error',
   },
 };
