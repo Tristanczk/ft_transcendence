85c3a507148aeecf4f5297f0c3e0326c3ebea550
Axel Brisse <axel.brisse@gmail.com>
2023-08-26 07:18:46 +0200

    two buttons the same width

--- frontend/mount/src/pages/HomePage.tsx
+++ frontend/mount/src/pages/HomePage.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { NAVBAR_HEIGHT } from '../constants';
 
 const GameButton = ({ mode }: { mode: string }) => (
-    <button className="py-4 px-8 bg-white border-4 border-black text-black text-2xl font-mono tracking-widest hover:bg-black hover:text-white transition duration-300">
+    <button className="flex justify-center items-center py-4 px-8 bg-white border-4 border-black text-black text-2xl font-mono tracking-widest hover:bg-black hover:text-white transition duration-300 w-1/2 max-w-xs">
         {mode}
     </button>
 );
@@ -10,10 +10,11 @@ const GameButton = ({ mode }: { mode: string }) => (
 const HomePage: React.FC = () => {
     return (
         <div
-            className="flex justify-center items-center bg-rose-600"
+            className="flex flex-col justify-center items-center bg-rose-600 space-y-4"
             style={{ height: `calc(100vh - ${NAVBAR_HEIGHT}px)` }}
         >
-            <GameButton mode="NEW GAME" />
+            <GameButton mode="CLASSIC" />
+            <GameButton mode="BATTLE ROYALE" />
         </div>
     );
 };
