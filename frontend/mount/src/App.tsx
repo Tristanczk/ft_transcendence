d03946aac89665cff4bce0cf1926e0477abd299a
Axel Brisse <axel.brisse@gmail.com>
2023-08-26 19:02:27 +0200

    y tabs

--- frontend/mount/src/App.tsx
+++ frontend/mount/src/App.tsx
@@ -21,25 +21,25 @@ const App: React.FC = () => {
     return (
         <WebsocketProvider value={socket}>
             <UserProvider>
-				<TrackingOnline />
-				<BrowserRouter>
-					<NavBar />
-					<Routes>
-						<Route path="/" Component={HomePage} />
-						<Route path="/battle" Component={BattlePage} />
-						<Route path="/classic" Component={ClassicPage} />
-						<Route path="/dashboard" Component={DashboardPage} />
-						<Route path="/chat" Component={ChatPage} />
-						<Route path="/signin" Component={SignInPage} />
-						<Route path="/signup" Component={SignUpPage} />
-						<Route path="/signin42" Component={SignInPage42} />
-						<Route path="/signout" Component={SignOutPage} />
-						<Route path="/settings" Component={SettingsPage} />
-						<Route path="/signin/result" Component={SignInResult} />
-						<Route path="*" Component={Page404} />
-					</Routes>
-				</BrowserRouter>
-			</UserProvider>
+                <TrackingOnline />
+                <BrowserRouter>
+                    <NavBar />
+                    <Routes>
+                        <Route path="/" Component={HomePage} />
+                        <Route path="/battle" Component={BattlePage} />
+                        <Route path="/classic" Component={ClassicPage} />
+                        <Route path="/dashboard" Component={DashboardPage} />
+                        <Route path="/chat" Component={ChatPage} />
+                        <Route path="/signin" Component={SignInPage} />
+                        <Route path="/signup" Component={SignUpPage} />
+                        <Route path="/signin42" Component={SignInPage42} />
+                        <Route path="/signout" Component={SignOutPage} />
+                        <Route path="/settings" Component={SettingsPage} />
+                        <Route path="/signin/result" Component={SignInResult} />
+                        <Route path="*" Component={Page404} />
+                    </Routes>
+                </BrowserRouter>
+            </UserProvider>
         </WebsocketProvider>
     );
 };
