a674cc909a4c5fc6ae255c6367d327f612f8a134
Lorenzo Simanic <lsimanic@student.42.fr>
2023-08-30 21:55:35 +0200

    prisma studio ok

--- backend/backend.sh
+++ backend/backend.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export DB_URL="postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@dev-db:${POSTGRES_PORT}/${POSTGRES_DB}?schema=public"
+export DATABASE_URL="postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@dev-db:${POSTGRES_PORT}/${POSTGRES_DB}?schema=public"
 
 npm install
 npx prisma generate
