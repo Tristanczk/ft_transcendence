9107a9c9467a847e2bccf84c0b7e6eb035d6b279
Nicolas LESAGE <nlesage@made-f0Dr11s12.clusters.42paris.fr>
2023-09-06 11:09:04 +0200

    tab for sockets with userId + socketId

--- backend/mount/src/gateway/gateway.service.ts
+++ backend/mount/src/gateway/gateway.service.ts
@@ -43,37 +43,52 @@ export class GatewayService implements OnModuleInit {
 
     async userArrive(body: PingProps) {
         // console.log('ft_arrive=' + body.id);
-        await this.prisma.user.update({
-            where: { id: body.id },
-            data: { isConnected: true },
-        });
-		const alreadyConnected = this.array.findIndex((a) => a.id === body.id);
-        this.array.push({
-            id: body.id,
-            idConnection: body.idConnection,
-            nb: 0,
-            date: Date.now(),
-        });
-        if (alreadyConnected === -1) {
-            this.server.emit('updateStatus', { idUser: body.id, type: 'come' });
+        try {
+            await this.prisma.user.update({
+                where: { id: body.id },
+                data: { isConnected: true },
+            });
+            const alreadyConnected = this.array.findIndex(
+                (a) => a.id === body.id,
+            );
+            this.array.push({
+                id: body.id,
+                idConnection: body.idConnection,
+                nb: 0,
+                date: Date.now(),
+            });
+            if (alreadyConnected === -1) {
+                this.server.emit('updateStatus', {
+                    idUser: body.id,
+                    type: 'come',
+                });
+            }
+        } catch (error) {
+            throw error;
         }
     }
 
     async userLeave(userId: number) {
-        console.log('ft_left=' + userId);
-		//implementer userLeave par onglet
-		// const nbConnexions = this.array.reduce((acc, cur) => a.id === body.id);
-        await this.prisma.user.update({
-            where: { id: userId },
-            data: { isConnected: false },
-        });
-        this.server.emit('updateStatus', { idUser: userId, type: 'leave' });
+        const nbConnexions = this.array.reduce(
+            (acc, cur) => acc + (cur.id === userId ? 1 : 0),
+            0,
+        );
+        // console.log('ft_left=' + userId + ', nb_tab_left=' + nbConnexions);
+        if (nbConnexions > 0) return;
+		try {
+			await this.prisma.user.update({
+				where: { id: userId },
+				data: { isConnected: false },
+			});
+			this.server.emit('updateStatus', { idUser: userId, type: 'leave' });	
+		}
+		catch (error) {
+			throw error
+		}
     }
 
     @SubscribeMessage('ping')
     async handlePing(@MessageBody() body: PingProps) {
-        console.log('get ping= ');
-        console.log(body);
         const id: number = body.id;
         if (id === -1) return;
         const l = this.array.findIndex(
@@ -87,19 +102,19 @@ export class GatewayService implements OnModuleInit {
         }
     }
 
-    @Interval(3000)
+    @Interval(1000)
     async handleInterval() {
         const timeNow: number = Date.now();
+        let idTmp: number = -1;
         this.array.forEach((arr) => {
             if (arr.id !== -1) {
                 if (timeNow - arr.date > 3000) {
-                    this.userLeave(arr.id);
+                    idTmp = arr.id;
                     arr.id = -1;
+                    this.userLeave(idTmp);
                 }
             }
         });
         this.array = this.array.filter((arr) => arr.id !== -1);
-        console.log('clients');
-        console.log(this.array);
     }
 }
