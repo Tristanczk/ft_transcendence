7311463582036e57c32d4e02a57df404d713a320
Axel Brisse <axel.brisse@gmail.com>
2023-09-14 14:30:27 +0200

    more efficient angleDist

--- shared/functions.ts
+++ shared/functions.ts
@@ -29,8 +29,8 @@ export const angleDist = (angle1: number, angle2: number) => {
     angle1 = trueMod(angle1, TAU);
     angle2 = trueMod(angle2, TAU);
     const d1 = angle1 - angle2;
-    const d2 = angle1 - angle2 + TAU;
-    const d3 = angle1 - angle2 - TAU;
+    const d2 = d1 + TAU;
+    const d3 = d1 - TAU;
     const ad1 = Math.abs(d1);
     const ad3 = Math.abs(d3);
     return ad1 <= d2 && ad1 <= ad3 ? d1 : d2 <= ad3 ? d2 : d3;
