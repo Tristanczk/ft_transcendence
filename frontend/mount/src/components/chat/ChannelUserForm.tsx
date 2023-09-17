3672623cc8600b85c0e9748a3fb6b00c4835989c
lorenzo <lorenzo.simanic@gmail.com>
2023-09-17 22:01:17 +0200

    fix setting bar

--- frontend/mount/src/components/chat/ChannelUserForm.tsx
+++ frontend/mount/src/components/chat/ChannelUserForm.tsx
@@ -12,24 +12,25 @@ export default function ChannelUserForm({
     handleClick: (idUser: number) => void;
     setChannelUsers: (users: UserSimplified[]) => void;
 }) {
-    if (!currentChannel) return <></>;
+    if (!currentChannel) return null;
 
     return (
         <>
             {channelUsers.length > 0 && (
-                <form>
+                <div id="list" className="flex flex-col space-y-4 overflow-y-auto scrollbar-thumb-gray-500 scrollbar-thumb-rounded scrollbar-track-gray-200 scrollbar-w-2 scrolling-touch overflow-clip shadow-lg transition-all duration-500 ease-in-out rounded-b-xl h-48 bg-gray-100">
                     {channelUsers.map((user) => (
                         <div
+                            key={user.id}
+                            className="friend-container flex items-center justify-center cursor-pointer bg-gray-100 hover:bg-gray-200 hover:scale-110 rounded-xl p-2 shadow-md transition-transform transform duration-200 ease-in-out"
                             onClick={() => {
                                 setChannelUsers([]);
                                 handleClick(user.id);
                             }}
-                            key={user.id}
                         >
-                            {user.nickname}
+                            <span className="friend-name text-gray-800 font-medium">{user.nickname}</span>
                         </div>
                     ))}
-                </form>
+                </div>
             )}
         </>
     );
